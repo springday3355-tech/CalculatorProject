26b49920b0481df6fa9866bef2f1ad88340efa35
송이 <springday3355@gmail.com>
2026-03-19 17:56:11 +0900

    스위치문 사용해 연산자 실행, default 사용해 잘못된 연산자 넣을경우 처음으로 돌아감

--- src/SelfTest.java
+++ src/SelfTest.java
@@ -11,11 +11,35 @@ public class SelfTest {
 
         System.out.println("사칙연산 기호를 입력하세요");
         char operator = sc.next().charAt(0);
+        int result = 0;
+        boolean success = true;
 
+        switch (operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                if (secondNum == 0) {
+                    System.out.println("나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다.");
+                    return;
+                }
+                result = firstNum / secondNum;
+                break;
 
+            default:
+                System.out.println("잘못된 연산 기호입니다.");
+                success = false;
 
-
-
-
+        }
+        if (success){
+            System.out.println("결과: "+ result);
+            System.out.println("---------------------");
+        }
     }
 }
