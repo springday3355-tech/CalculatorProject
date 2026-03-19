c91d2bac17852337189b63f11d224cf816ccbf6f
송이 <springday3355@gmail.com>
2026-03-19 17:14:16 +0900

    연산 오류 정제 출력, 제어문 사용

--- src/SelfCalculator.java
+++ src/SelfCalculator.java
@@ -8,11 +8,39 @@ public class SelfCalculator {
         int firstNum = sc.nextInt();
 
         System.out.println("두번째 숫자를 입력하세요: ");
-        int secondName = sc.nextInt();
+        int secondNum = sc.nextInt();
 
         System.out.println("사칙 연산자를 입력하세요: ");
         char operator = sc.next().charAt(0);
 
-        }
+        int result = 0;
+        boolean success = true;
 
+        switch (operator) {
+            case '+':
+                result = firstNum + secondNum;
+                break;
+            case '-':
+                result = firstNum - secondNum;
+                break;
+            case '*':
+                result = firstNum * secondNum;
+                break;
+            case '/':
+                if (secondNum == 0) {
+                    System.out.println("0이 입력될 수 없습니다.");
+                    return;
+                }
+                result = firstNum / secondNum;
+                break;
+
+            default:
+                System.out.println("잘못된 연산 기호입니다.");
+                success = false;
+        }
+        if (success){
+            System.out.println("결과: " + result);
+            System.out.println("-------------------");
+        }
     }
+}
