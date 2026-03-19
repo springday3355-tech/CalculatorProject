be67fe4295670dfc95ffaff69c82946d33dc2406
송이 <springday3355@gmail.com>
2026-03-19 17:44:43 +0900

    양의 정수 선언

---
+++ src/SelfTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SelfTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
+
+        System.out.println("사칙연산 기호를 입력하세요");
+
+
+
+    }
+}
