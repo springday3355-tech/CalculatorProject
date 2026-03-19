dca6c1afdbc60eaef1e873f625712ba1cf21500e
송이 <springday3355@gmail.com>
2026-03-19 16:59:45 +0900

    양의정수 입력받기

---
+++ src/SelfCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SelfCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+
+        System.out.println("두번째 숫자를 입력하세요: ");
+        int secondName = sc.nextInt();
+
+        System.out.println("사칙 연산자를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
+        }
+
+    }
