a460a169ffbfc587b7e90bb9c8a38dc2a8ac69eb
송이 <springday3355@gmail.com>
2026-03-25 17:35:14 +0900

    삭제,조회 메서드 추가

--- src/firstcalculator/ArithmeticCalculator.java
+++ src/firstcalculator/ArithmeticCalculator.java
@@ -2,13 +2,16 @@ package firstcalculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ArithmeticCalculator <T extends Number> {
-    private List<Double> results =new ArrayList<>();
+    private List<Double> results = new ArrayList<>();
+
     public double calculate(T num1, T num2, OperatorType operator) {
         double result = 0;
 
-        switch (operator){
+        switch (operator) {
             case ADD:
                 result = num1.doubleValue() + num2.doubleValue();
                 break;
@@ -26,4 +29,24 @@ public class ArithmeticCalculator <T extends Number> {
         return result;
     }
 
+    public void printResultsGreaterThan(double threshold) {
+        List<Double> num = results.stream()
+                .filter(n -> n > threshold) // 1.기준보다 큰 것만 필터링
+                .collect(Collectors.toList()); // 2. 리스트로 수집
+        System.out.println(threshold + "보다 큰 결과들: " + num);
+    }
+
+    public List<Double> getResults() {
+        return results;
+    }
+
+    public void removeFirstResult() {
+        if (!results.isEmpty()) {
+            results.remove(0);
+            System.out.println("가장 오래된 연산 결과가 삭제 되었습니다.");
+        } else {
+            System.out.println("삭제할 결과가 없습니다.");
+
+        }
+    }
 }
